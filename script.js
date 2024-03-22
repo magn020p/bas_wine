cadbf24f537b2f786cd89177c209c579b845cac2
Magnus Bolton <magnus@bolton.dk>
2024-03-22 10:47:26 +0100

    nye opdateringer

--- script.js
+++ script.js
@@ -9,6 +9,35 @@
 //     nav.classList.remove("active");
 //   });
 // });
+
+window.onload = function () {
+  var slides = document.getElementsByClassName("carousel-item"),
+    addActive = function (slide) {
+      slide.classList.add("active");
+    },
+    removeActive = function (slide) {
+      slide.classList.remove("active");
+    };
+  addActive(slides[0]);
+
+  setInterval(function () {
+    for (var i = 0; i < slides.length; i++) {
+      if (i + 1 == slides.length) {
+        addActive(slides[0]);
+        slides[0].style.zIndex = 100;
+        setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+        break;
+      }
+      if (slides[i].classList.contains("active")) {
+        slides[i].removeAttribute("style");
+        setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+        addActive(slides[i + 1]);
+        break;
+      }
+    }
+  }, 4000);
+};
+
 $("#menu li").click(function (e) {
   e.preventDefault(); //Remove this in your main code
   $("#menu li").removeClass("active");
